38318d14766ea1e696c8c450891dae19d00d6d95
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-11-13 18:48:06 +0100

    feat: add Buttons components

---
+++ src/Components/common/crudButtons/Save.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button } from 'antd';
+
+
+class SaveButton extends React.Component {
+
+    render () {
+
+        return (
+            <Button type="primary" key={"save"}>Save</Button>
+        )
+    }
+}
+
+export default SaveButton;
