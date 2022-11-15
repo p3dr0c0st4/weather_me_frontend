c7ec3683eded976eaceaf14eebc5e372524762bc
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-11-15 21:56:46 +0100

    refactor: change crudButtons to descriptive name

---
+++ src/components/crudButtons/SaveButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'antd';
+
+export default () => {
+    return (
+        <Button type="primary" key={'save'}>
+            Save
+        </Button>
+    );
+};
