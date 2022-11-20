1854e02e9e3319da06d296a599d01a706c9d98eb
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-11-20 17:09:11 +0100

    feat: add HumidityPage

---
+++ src/pages/humidity/HumidityPage.tsx
@@ -0,0 +1,21 @@
+import { Col, Divider, Row } from 'antd';
+import React from 'react';
+import ContainerList from './../../components/ContainerList';
+import CreateButton from './../../components/crudButtons/CreateButton';
+
+export default () => {
+    return (
+        <>
+            <Divider orientation="left">Humidity</Divider>
+            <Row>
+                <Col push={22} span={4}>
+                    <CreateButton key={'create'} />
+                </Col>
+
+                <Col span={24}>
+                    <ContainerList />
+                </Col>
+            </Row>
+        </>
+    );
+};
