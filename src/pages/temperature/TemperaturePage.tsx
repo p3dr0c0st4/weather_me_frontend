b0132d9e7a26cf1577218f986ce22e45c26f9827
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-11-20 18:47:26 +0100

    feat: center divider on  TemperaturePage

--- src/pages/temperature/TemperaturePage.tsx
+++ src/pages/temperature/TemperaturePage.tsx
@@ -5,8 +5,8 @@ import CreateButton from './../../components/crudButtons/CreateButton';
 
 export default () => {
     return (
-        <>
-            <Divider orientation="left">Temperature</Divider>
+        <> 
+            <Divider orientation="center">Temperature</Divider>
             <Row>
                 <Col push={22} span={4}>
                     <CreateButton key={'create'} />
