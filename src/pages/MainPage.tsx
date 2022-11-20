cfa924577e052c8a98d069c72ae9d7fd627d7aa1
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-11-20 17:07:49 +0100

    feat: add MainPage component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import Background from '../components/background';
 
 export default () => {      
         return (
-
-            <>Main Page</>
+            <Background />
         );
     
 }
