7e0f0f123bc5807dbe697e796536a5dc4f2b192d
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-11-20 17:08:11 +0100

    feat: add background to MainPage

---
+++ src/components/background.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Image } from 'antd';
+import background from './../assets/images/background.png'
+
+
+export default () => {
+    return (
+  <Image
+    width={'100%'}
+    height={'100%'}
+    src={"https://blog.depositphotos.com/wp-content/uploads/2017/07/Soothing-nature-backgrounds-2.jpg?x-oss-process=image/blur,r_50,s_50/"}
+    preview={false}
+  />
+  )
+};
