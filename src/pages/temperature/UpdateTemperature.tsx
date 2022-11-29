9350d92a8a9c20f35d5a421fa388e15f3cfc53c5
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-11-29 21:14:44 +0100

    feat: add axios update

--- src/pages/temperature/UpdateTemperature.tsx
+++ src/pages/temperature/UpdateTemperature.tsx
@@ -1,14 +1,38 @@
-import { Button, Space } from 'antd';
-import React from 'react';
+import React  from 'react';
+import axios from 'axios';
 import CreateForm from './../../components/CreateForm';
+import { Button, Space } from 'antd';
 import {useParams} from "react-router-dom";
 
+
+
+
+
+
 export default () => {
+
+
     const { id } = useParams();
-    const onUpdate=()=>{
+
+    // const [temperature, setTemperature] = useState({});
+    // const temp = temperature
+
+    const onUpdate =  ()=>{
+         axios.patch(`/temperature/update/${id}`)
+        .then(res => {
+            console.log('responseLog');
+            return res.data;
+            console.log(res.data);
+        })
+        .catch(err => {
+            console.log('errorLog: '+err)
+        })
+        
         //TODO: call API with id to update
         console.log('UPDATED', id)
     }
+
+
     return (
         <Space direction="vertical">
             {id}
