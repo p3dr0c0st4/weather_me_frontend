77bed7da8fd577848792056054cc6273be1af6f2
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-11-29 21:15:15 +0100

    feat: add axios get

--- src/pages/temperature/CreateTemperature.tsx
+++ src/pages/temperature/CreateTemperature.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Button, Space } from 'antd';
 import CreateForm from './../../components/CreateForm';
+import axios from 'axios';
 
 export default () => {
-    const onSaveClick = ()=>{
+    const onSaveClick =  ()=>{
+        
         //TODO: call api with create
         console.log('CREATE')
+         axios.get(`http://localhost:3000/temperature/create`)
+                .then(res => {
+                    console.log('responseLog');
+                    return res.data;
+                    console.log(res.data);
+                })
+                .catch(err => {
+                    console.log('errorLog: '+err)
+                })
     }
     return (
         <Space direction="vertical">
