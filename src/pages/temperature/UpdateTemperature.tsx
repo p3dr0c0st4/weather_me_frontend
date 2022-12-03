7e6c60adc2c9c1a705ca4825e7264abc4f2d3b60
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-12-03 16:14:20 +0100

    refactor: moved api call to temperatureService
    Co-authored-by: PɤCo <p3dr0c0st4@users.noreply.github.com>

--- src/pages/temperature/UpdateTemperature.tsx
+++ src/pages/temperature/UpdateTemperature.tsx
@@ -1,42 +1,26 @@
 import React  from 'react';
-import axios from 'axios';
-import CreateForm from './../../components/CreateForm';
 import { Button, Space } from 'antd';
 import {useParams} from "react-router-dom";
-
-
-
-
+import { updateItem } from '../../services/TemperatureService';
 
 
 export default () => {
 
+    const onUpdate = () =>{
+        updateItem('123', {
+            id: '12',
+            location: 'casa',
+            date:10,
+            temperature: 20,
+        })
+    }
 
     const { id } = useParams();
-
-    // const [temperature, setTemperature] = useState({});
-    // const temp = temperature
-
-    const onUpdate =  ()=>{
-         axios.patch(`/temperature/update/${id}`)
-        .then(res => {
-            console.log('responseLog');
-            return res.data;
-            console.log(res.data);
-        })
-        .catch(err => {
-            console.log('errorLog: '+err)
-        })
-        
-        //TODO: call API with id to update
-        console.log('UPDATED', id)
-    }
 
 
     return (
         <Space direction="vertical">
             {id}
-            <CreateForm />
             <Button  key='update' type='default' onClick={onUpdate}>
                 Update
             </Button>
