8a6bdc71fb3885975e98807cfe15e5018bb74441
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-12-20 16:05:46 +0100

    feat: add component render condition with 2buttons

--- src/pages/humidity/HumidityPage.tsx
+++ src/pages/humidity/HumidityPage.tsx
@@ -1,6 +1,8 @@
-import { Button, Col, Divider, Row } from 'antd';
-import React from 'react';
+import { AreaChartOutlined, UnorderedListOutlined } from '@ant-design/icons';
+import { Button, Col, Divider, Row, Space } from 'antd';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import GraphHumidity from './GraphHumidity';
 import ListHumidity from './ListHumidity';
 
 export default () => {
@@ -8,18 +10,41 @@ export default () => {
     const onCreateClick = ()=>{
         navigate('/humidity/create');
     }
+
+    const [listPage, setListPage] = useState(true);
+    
     return (
         <>
-            <Divider orientation="center">Humidity</Divider>
+            
+            <Divider>Humidity</Divider>
+            
             <Row>
-                <Col push={22} span={4}>
+                <Col span={8}></Col>
+                <Col span={8} style={{ textAlign: 'center'}}>
+                    <Space size={'large'}>
+                        <Button key={'list'} onClick={() => {setListPage(true)}}>
+                            <UnorderedListOutlined  style={{ fontSize:'1.4em'}} />
+                        </Button>
+                        <Button key={'graph'} onClick={() => {setListPage(false)}}>
+                            <AreaChartOutlined style={{ fontSize:'1.4em'}} />
+                        </Button>
+                    
+                    </Space>
+                </Col>
+                <Col span={8} push={6}>
                 <Button  key='create' type='primary' onClick={onCreateClick}>
                     Create
                 </Button>
                 </Col>
-
+            
+            </Row>
+            <Divider ></Divider>
+            <Row>
                 <Col span={24} >
-                    <ListHumidity />
+                    {listPage
+                        ? <ListHumidity />
+                        : <GraphHumidity/>
+                    }
                 </Col>
             </Row>
         </>
