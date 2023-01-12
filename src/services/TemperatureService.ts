55cb2eb9ae50ba216e06da4268ebb1de3cfdcc77
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2023-01-12 13:32:37 +0100

    feat: add jwt cookie to authorization headers

--- src/services/TemperatureService.ts
+++ src/services/TemperatureService.ts
@@ -1,15 +1,24 @@
 import axios from 'axios';
 import { TemperatureDto } from './dtos/TemperatureDto';
+import { jwtCookie } from './middleware/token';
 
-export const createTemperatureItem = async (data:TemperatureDto): Promise<boolean> => {
+
+
+
+
+export const createTemperatureItem = async (data: TemperatureDto): Promise<boolean> => {
+    //fetch validation token from cookie
+    const token: string = jwtCookie();
+
     try {
-            const body = JSON.stringify(data)
-            const resp = await axios.post(`${process.env.REACT_APP_API_URL}/temperature`,body,{
-                headers:{
-                    'content-type': 'application/json'
-                }
-            });
-            return resp.data.success
+        const body = JSON.stringify(data)
+        const resp = await axios.post(`${process.env.REACT_APP_API_URL}/temperature`, body, {
+            headers: {
+                'content-type': 'application/json',
+                'authorization': token
+            }
+        });
+        return resp.data.success
     } catch (error) {
         console.log(error)
         return false;
@@ -17,26 +26,34 @@ export const createTemperatureItem = async (data:TemperatureDto): Promise<boolea
 }
 
 export const listAllTemperature = async (): Promise<TemperatureDto[]> => {
+    //fetch validation token from cookie
+    const token: string = jwtCookie();
+
     try {
-            const resp = await axios.get(`${process.env.REACT_APP_API_URL}/temperature`,{
-                headers:{
-                    'content-type': 'application/json'
-                }
-            });
-            return resp.data.data
+        const resp = await axios.get(`${process.env.REACT_APP_API_URL}/temperature`, {
+            headers: {
+                'content-type': 'application/json',
+                'authorization': token
+            }
+        });
+        return resp.data.data
     } catch (error) {
         console.log(error)
         return [];
     }
 }
-export const getTemperatureItem = async (id:string): Promise<TemperatureDto | null> => {
+export const getTemperatureItem = async (id: string): Promise<TemperatureDto | null> => {
+    //fetch validation token from cookie
+    const token: string = jwtCookie();
+
     try {
-            const resp = await axios.get(`${process.env.REACT_APP_API_URL}/temperature/${id}`,{
-                headers:{
-                    'content-type': 'application/json'
-                }
-            });
-            return resp.data.data
+        const resp = await axios.get(`${process.env.REACT_APP_API_URL}/temperature/${id}`, {
+            headers: {
+                'content-type': 'application/json',
+                'authorization': token
+            }
+        });
+        return resp.data.data
     } catch (error) {
         console.log(error)
         return null
@@ -44,15 +61,19 @@ export const getTemperatureItem = async (id:string): Promise<TemperatureDto | nu
 }
 
 
-export const updateTemperatureItem = async (id:string, data:TemperatureDto): Promise<boolean> => {
+export const updateTemperatureItem = async (id: string, data: TemperatureDto): Promise<boolean> => {
+    //fetch validation token from cookie
+    const token: string = jwtCookie();
+
     try {
-            const body = JSON.stringify(data)
-            const resp = await axios.patch(`${process.env.REACT_APP_API_URL}/temperature/${id}`,body,{
-                headers:{
-                    'content-type': 'application/json'
-                }
-            });
-            return resp.data.success
+        const body = JSON.stringify(data)
+        const resp = await axios.patch(`${process.env.REACT_APP_API_URL}/temperature/${id}`, body, {
+            headers: {
+                'content-type': 'application/json',
+                'authorization': token
+            }
+        });
+        return resp.data.success
 
     } catch (error) {
         console.log(error)
@@ -60,14 +81,18 @@ export const updateTemperatureItem = async (id:string, data:TemperatureDto): Pro
     }
 }
 
-export const deleteTemperatureItem = async (id:string): Promise<boolean> => {
+export const deleteTemperatureItem = async (id: string): Promise<boolean> => {
+    //fetch validation token from cookie
+    const token: string = jwtCookie();
+    
     try {
-            const resp = await axios.delete(`${process.env.REACT_APP_API_URL}/temperature/${id}`,{
-                headers:{
-                    'content-type': 'application/json'
-                }
-            });
-            return resp.data.success
+        const resp = await axios.delete(`${process.env.REACT_APP_API_URL}/temperature/${id}`, {
+            headers: {
+                'content-type': 'application/json',
+                'authorization': token
+            }
+        });
+        return resp.data.success
     } catch (error) {
         console.log(error)
         return false;
