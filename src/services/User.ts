0210b6cb549872620d420e5fb61bc04afa9dcd46
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2023-01-22 16:16:30 +0100

    fix: changed login path

--- src/services/User.ts
+++ src/services/User.ts
@@ -1,31 +1,18 @@
 import axios from 'axios';
 import { LoginDto } from './dtos/LoginDto';
-import Cookies from 'universal-cookie';
 
 
 export const login = async (data: LoginDto): Promise<boolean> => {
 
-    const cookies = new Cookies();
 
     try {
-        const body = JSON.stringify(data)
-        const res = await axios.post(`http://localhost:3000/login`, body, {
+        const body = JSON.stringify(data) //http://localhost:3000/api/v1/user/login
+        const res = await axios.post(`${process.env.REACT_APP_API_URL}/user/login`, body, {
+            withCredentials: true,
             headers: {
                 'content-type': 'application/json'
             }
-        });
-
-
-        
-            const token = res.data.token;
-            
-
-            cookies.set('jwt_token', token,
-                {
-                    expires: new Date(token.exp * 100000)
-                }
-            );
-
+        })
 
             return res.data.success
         
